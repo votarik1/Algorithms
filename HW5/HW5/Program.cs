313556988fb56e70ce895a2d73501f1201820976
Alex <alexspb41@gmail.com>
2021-06-26 02:08:56 +0300

    ADD HW5

---
+++ HW5/HW5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tree<int> myTree = new Tree<int>();
+            for (int i = 0; i < 15; i++)
+            {
+                myTree.Add(i);
+            }
+
+            myTree.PrintTop4();
+
+
+
+
+
+            myTree.BFSSearch(14);
+
+            Console.WriteLine();
+
+            myTree.DFSSearch(14);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
